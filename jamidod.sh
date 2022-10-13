b5226d8afe49a3944e4bbeed17761bce610bd9df
Jamidod <115701547+Jamidod@users.noreply.github.com>
2022-10-13 16:58:25 +0700

    Add files via upload

---
+++ jamidod.sh
@@ -0,0 +1,42 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By jamidod
+echo $green"selamat datang"
+echo
+sleep 2
+echo $yellow"silahkan pilih tools nya"
+echo
+echo $cyan"1.)stabilkan jaringan"
+echo
+echo $blue"0.)keluar aja !!!"
+echo
+echo $white
+read -p "pilihMana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay jamidod
+echo $green"stabilkan jaringan by jamidod"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "bye"
+echo $white"To be continued"
+sleep 3
+exit
+fi
+
